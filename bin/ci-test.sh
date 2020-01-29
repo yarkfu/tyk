4e45b1a401e219b5ce6d832c962732108bb03cc7
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-29 08:42:06 +0000

    Fix CI failure from 'grep -v'.

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -20,21 +20,31 @@ fi
 
 race=""
 if [[ -n ${LATEST_GO:-} ]]; then
-  fmt_files=$(gofmt -l . | grep -v vendor)
+  # Filtering files with 'grep -v' would return a non-zero exit status
+  fmt_files=$(
+    find . -type f -name "*.go" -not -path "./vendor/*" -print0 \
+      | xargs -0 gofmt -l
+  )
   if [[ -n $fmt_files ]]; then
     fatal "Run 'gofmt -w' on these files:\n$fmt_files"
   fi
 
   echo "gofmt is OK!"
 
-  imp_files="$(goimports -l . | grep -v vendor)"
+  # .goimportsignore is not granular enough to ignore individual files in a given directory.
+  # In this case, generated protobuf code in *.pb.go files.
+  # Ref: https://godoc.org/golang.org/x/tools/cmd/goimports
+  imp_files=$(
+    find . -type f -name "*.go" -not -name "*.pb.go" -not -path "./vendor/*" -print0 \
+      | xargs -0 goimports -l
+  )
   if [[ -n $imp_files ]]; then
     fatal "Run 'goimports -w' on these files:\n$imp_files"
   fi
 
   echo "goimports is OK!"
 
-  # Run with race if latest
+  # Build goplugins with -race if latest
   race="-race"
 fi
 
@@ -44,7 +54,7 @@ go get -t
 
 export PKG_PATH=$GOPATH/src/github.com/TykTechnologies/tyk
 
-# build Go-plugin used in tests
+# Build goplugins used in tests
 go build "$race" -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins \
   || fatal "Building goplugins failed"
 
@@ -59,15 +69,21 @@ for pkg in $pkgs; do
     tags+=("-tags 'goplugin'")
   fi
 
-  race=""
+  # Build up an array of arguments to pass to 'go test'
+  test_args=(
+    -v
+    "-coverprofile=test.cov"
+    "-timeout=$test_timeout"
+  )
 
-  # Some tests should not be run with -race. Therefore, test them with penultimate Go version.
-  # And, test with -race in latest Go version.
+  # Some tests should not be run with -race.
+  # Therefore, test them with penultimate Go version.
+  # And test with -race in latest Go version.
   if [[ -n $LATEST_GO && $skip_race == false ]]; then
-    race="-race"
+    test_args+=(-race)
   fi
 
-  show go test -v "$race" -timeout "$test_timeout" -coverprofile=test.cov "$pkg" "${tags[@]}" \
+  show go test "${test_args[@]}" "${tags[@]}" "$pkg" \
     || fatal "go test failed"
   show go vet "${tags[@]}" "$pkg" \
     || fatal "go vet errored"
