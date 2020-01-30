af944b17209b9eeccb6518bd5cab8e819b573cd4
James Lucktaylor <jlucktay+github@gmail.com>
2020-01-30 17:12:26 +0000

    Add plugins, skip race logic, etc.

--- images/test-env/data/test.sh
+++ images/test-env/data/test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+function fatal() {
+  printf "%s\n" "$@" >&2
+  exit 1
+}
+
 if [ -z "${GOPATH:-}" ]; then
   fatal "GOPATH is not defined"
 fi
@@ -9,24 +14,36 @@ test_timeout=5m
 
 # TODO(jlucktay): add linters
 
-cd /go/src/github.com/TykTechnologies/tyk
-
 pkgs="$(go list ./...)"
 
 go get -t
 
+export PKG_PATH=$GOPATH/src/github.com/TykTechnologies/tyk
+
+# Build goplugins used in tests
+go build -race -o ./test/goplugins/goplugins.so -buildmode=plugin ./test/goplugins \
+  || fatal "Building goplugins failed"
+
 for pkg in $pkgs; do
   tags=
 
-  if [[ $pkg == *"goplugin" ]]; then
+  # TODO: Remove skip_race variable after solving race conditions in tests.
+  skip_race=0
+  if [[ $pkg == *"grpc" ]]; then
+    skip_race=1
+  elif [[ $pkg == *"goplugin" ]]; then
     tags="-tags=goplugin"
   fi
 
+  # Build up an array of arguments to pass to 'go test'
   test_args=(
-    -coverprofile test.cov
     -timeout "$test_timeout"
     -v
   )
+
+  if [ "$skip_race" -eq 0 ]; then
+    test_args+=(-race)
+  fi
 
   set -x
   go test "${test_args[@]}" "$tags" "$pkg"
